c2f269b16436ffad7547deaf6c4dfba43c40dee0
MOSTAFA837 <mostafamanchester837@gmail.com>
2021-12-15 11:52:43 +0200

    react category filter

---
+++ precache-manifest.fe0daf8bb4394c7740a98a8aaaf9f702.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c28e33130d093c287ac55ed0b05304f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "29b6a5ef0b1c9ca4412f",
+    "url": "/static/css/main.26e112f5.chunk.css"
+  },
+  {
+    "revision": "6e331cfd5d1a1283573a",
+    "url": "/static/js/2.66b87b03.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.66b87b03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29b6a5ef0b1c9ca4412f",
+    "url": "/static/js/main.9053cf38.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  }
+]);
